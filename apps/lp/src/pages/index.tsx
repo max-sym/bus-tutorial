bcfd37ab1b0f6350c3e38626310ebfe34dfa3056
Max <mymaksym@gmail.com>
2022-04-25 17:19:13 +0100

    improve hero section

--- apps/lp/src/pages/index.tsx
+++ apps/lp/src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Layout, Seo } from "../components"
 import tw from "tailwind-styled-components"
 
-const Title = tw.span`text-5xl text-green-500 uppercase`
+const Title = tw.span`text-5xl text-green-500 uppercase font-bold`
 const Caption = tw.span`text-3xl text-gray-900 uppercase font-bold`
 const Description = tw.h1`text-2xl text-gray-500`
 const MainImage = tw.img`rounded-3xl shadow-lg`
@@ -12,6 +12,36 @@ const Separator = tw.div`w-10 h-1 bg-blue-500`
 
 const Button = tw.button`bg-green-500 px-5 py-3 rounded-xl text-white font-bold uppercase`
 const Gradient = tw.div`absolute inset-0 bg-gradient-radial-hero`
+
+const heroFeatures = [
+  {
+    title: "Book Easy",
+    description: "Tickets with QR Code",
+  },
+  {
+    title: "Discounts",
+    description: "For those who create an account",
+  },
+  {
+    title: "30+",
+    description: "Cities around the U.S.",
+  },
+]
+
+const HeroFeatureItem = ({ item }) => (
+  <div>
+    <h5 className="font-bold">{item.title}</h5>
+    <p>{item.description}</p>
+  </div>
+)
+
+const HeroFeatures = () => (
+  <div className="flex gap-4">
+    {heroFeatures.map(item => (
+      <HeroFeatureItem item={item} />
+    ))}
+  </div>
+)
 
 const LogoTitle = () => (
   <div>
@@ -27,14 +57,19 @@ const LogoTitle = () => (
 )
 
 const TextSection = () => (
-  <div className="space-y-6">
-    <LogoTitle />
-    <Description>
-      {
-        "This is a private bus company that takes you from point A to point B within the Continental US."
-      }
-    </Description>
-    <Button>{"Create Account"}</Button>
+  <div>
+    <div className="space-y-6">
+      <LogoTitle />
+      <Description>
+        {
+          "This is a private bus company that takes you from point A to point B within the Continental US."
+        }
+      </Description>
+      <Button>{"Create Account"}</Button>
+    </div>
+    <div className="mt-32">
+      <HeroFeatures />
+    </div>
   </div>
 )
 
