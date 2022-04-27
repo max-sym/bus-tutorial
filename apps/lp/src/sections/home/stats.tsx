23f60d2baa1721309d2d7039798d35d3da8458d7
Max <mymaksym@gmail.com>
2022-04-27 11:58:35 +0100

    animate stats section

--- apps/lp/src/sections/home/stats.tsx
+++ apps/lp/src/sections/home/stats.tsx
@@ -4,55 +4,82 @@ import tw from "tailwind-styled-components"
 import { RiBuilding2Fill } from "@react-icons/all-files/ri/RiBuilding2Fill"
 import { RiVipFill } from "@react-icons/all-files/ri/RiVipFill"
 import { BsPersonCheckFill } from "@react-icons/all-files/bs/BsPersonCheckFill"
+import { InView } from "react-cool-inview"
+import CountUp from "react-countup"
 
-const CardContainer = tw.div`flex justify-center gap-4 mt-14`
+const CardsContainer = tw.div`flex justify-center gap-4 mt-14`
 
 const stats = [
   {
     title: "Passengers Served",
-    quantity: 10_503_334,
+    value: 10_503_334,
     icon: BsPersonCheckFill,
     color: "text-blue-500",
   },
   {
     title: "Stations around the U.S.",
-    quantity: 1325,
+    value: 1_325,
     icon: RiVipFill,
     color: "text-green-500",
   },
   {
     title: "VIP served",
-    quantity: 3209,
+    value: 3_209,
     icon: RiBuilding2Fill,
     color: "text-blue-500",
   },
 ]
 
-const StatItem = ({ item }) => (
+const StatItem = ({ item, inView }: any) => (
   <Card>
-    <CardContent className="flex flex-col justify-center items-center">
+    <CardContent className="flex flex-col items-center justify-center">
       <item.icon className={`w-8 h-8 ${item.color}`} />
-      <Text variant="h4" className={item.color}>
-        {item.quantity}
-      </Text>
-      <Text variant="bodyBig" className="mt-2 px-8">
+      <CountUp
+        start={0}
+        delay={8}
+        duration={8}
+        separator=","
+        end={inView ? item.value : 0}
+      >
+        {({ countUpRef }) => (
+          <Text variant="h4" className={item.color} ref={countUpRef} />
+        )}
+      </CountUp>
+      <Text variant="bodyBig" className="px-8 mt-2">
         {item.title}
       </Text>
     </CardContent>
   </Card>
 )
 
-export const StatsSection = () => {
-  return (
-    <SectionAndOffset>
-      <Text variant="h4" className="text-center uppercase">
-        {"Numbers Say It All"}
-      </Text>
-      <CardContainer>
-        {stats.map(item => (
-          <StatItem key={item.title} item={item} />
-        ))}
-      </CardContainer>
-    </SectionAndOffset>
-  )
-}
+export const StatsSectionCore = ({ observe, inView }: any) => (
+  <SectionAndOffset ref={observe}>
+    <Text
+      variant="h4"
+      className={`text-center uppercase transition duration-1000 delay-150 ${
+        inView ? "" : "opacity-0"
+      }`}
+    >
+      {"Numbers Say It All"}
+    </Text>
+    <CardsContainer>
+      {stats.map((item, index) => (
+        <div
+          key={item.title}
+          className={`transition duration-1000 ${inView ? "" : "opacity-0"}`}
+          style={{
+            transitionDelay: 300 + 150 * index + "ms",
+          }}
+        >
+          <StatItem item={item} inView={inView} />
+        </div>
+      ))}
+    </CardsContainer>
+  </SectionAndOffset>
+)
+
+export const StatsSection = () => (
+  <InView unobserveOnEnter>
+    <StatsSectionCore />
+  </InView>
+)
