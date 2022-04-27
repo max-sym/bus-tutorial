448727f890d63862bb40ac611404b3543a06bca8
Max <mymaksym@gmail.com>
2022-04-27 19:13:57 +0100

    adjust color palette to slate

--- apps/lp/tailwind.config.js
+++ apps/lp/tailwind.config.js
@@ -1,7 +1,22 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{html,js,ts,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.slate,
+      green: colors.green,
+      red: colors.red,
+      blue: colors.blue,
+      purple: colors.purple,
+      yellow: colors.yellow,
+      pink: colors.pink,
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
