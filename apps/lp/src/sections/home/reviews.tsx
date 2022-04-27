a325f77a9693708701bb036ba5646704d8866331
Max <mymaksym@gmail.com>
2022-04-27 12:22:31 +0100

    animate reviews section

--- apps/lp/src/sections/home/reviews.tsx
+++ apps/lp/src/sections/home/reviews.tsx
@@ -3,8 +3,9 @@ import React from "react"
 import tw from "tailwind-styled-components"
 import { BsStarFill } from "@react-icons/all-files/bs/BsStarFill"
 import { IoMdQuote } from "@react-icons/all-files/io/IoMdQuote"
+import { InView } from "react-cool-inview"
 
-const CardContainer = tw.div`flex justify-center gap-4 mt-14`
+const CardsContainer = tw.div`flex justify-center gap-4 mt-14`
 const ImageContainer = tw.div`rounded-full overflow-hidden w-14 h-14`
 const Star = tw(BsStarFill)`w-4 h-4 text-yellow-500`
 
@@ -82,17 +83,34 @@ const ReviewItem = ({ item }: { item: ReviewType }) => {
   )
 }
 
-export const ReviewsSection = () => {
-  return (
-    <SectionAndOffset>
-      <Text variant="h4" className="text-center uppercase">
-        {"What Our Clients Say"}
-      </Text>
-      <CardContainer>
-        {reviews.map(item => (
-          <ReviewItem key={item.name} item={item} />
-        ))}
-      </CardContainer>
-    </SectionAndOffset>
-  )
-}
+export const ReviewsSectionCore = ({ inView, observe }: any) => (
+  <SectionAndOffset ref={observe}>
+    <Text
+      variant="h4"
+      className={`text-center uppercase transition duration-1000 delay-150 ${
+        inView ? "" : "opacity-0"
+      }`}
+    >
+      {"What Our Clients Say"}
+    </Text>
+    <CardsContainer>
+      {reviews.map((item, index) => (
+        <div
+          key={item.name}
+          className={`transition duration-1000 ${inView ? "" : "opacity-0"}`}
+          style={{
+            transitionDelay: 300 + 150 * index + "ms",
+          }}
+        >
+          <ReviewItem item={item} />
+        </div>
+      ))}
+    </CardsContainer>
+  </SectionAndOffset>
+)
+
+export const ReviewsSection = () => (
+  <InView unobserveOnEnter>
+    <ReviewsSectionCore />
+  </InView>
+)
