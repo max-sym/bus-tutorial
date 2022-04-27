c3be78e5605cb66d0af30053520eaf6e597d07dd
Max <mymaksym@gmail.com>
2022-04-27 11:45:27 +0100

    animate midsection

--- apps/lp/src/sections/home/mid.tsx
+++ apps/lp/src/sections/home/mid.tsx
@@ -1,20 +1,26 @@
 import { SectionAndOffset, Text } from "components"
 import React from "react"
+import { InView } from "react-cool-inview"
 import tw from "tailwind-styled-components"
 
 const Column = tw.div`w-1/2`
-const ImageContainer = tw.div`rounded-3xl overflow-hidden h-[600px] shadow-xl`
-const Image = tw.img`w-full h-full object-cover`
+const ImageContainer = tw.div`rounded-3xl overflow-hidden h-[600px] shadow-xl transition ease-in-out duration-[2000ms] delay-1000`
+const Image = tw.img`w-full h-full object-cover transition transform duration-[12000ms] ease-out`
+const TopPartContainer = tw.div`transition duration-1000 delay-150`
 
-const TopPart = ({ section }) => (
-  <>
+const Container = tw.div`flex justify-between mt-20 gap-12
+${({ direction }) => (direction === "left" ? "flex-row" : "flex-row-reverse")}
+`
+
+const TopPart = ({ section, observe, inView }: any) => (
+  <TopPartContainer ref={observe} className={`${inView ? "" : "opacity-0"}`}>
     <Text variant="h4" className="text-center uppercase">
       {section.title}
     </Text>
-    <Text variant={"bodyBig"} className="text-center w-1/2 mx-auto mt-8">
+    <Text variant={"bodyBig"} className="w-1/2 mx-auto mt-8 text-center">
       {section.description}
     </Text>
-  </>
+  </TopPartContainer>
 )
 
 const FeatureItem = ({ feature }) => (
@@ -29,27 +35,52 @@ const FeatureItem = ({ feature }) => (
   </div>
 )
 
-const Container = tw.div`flex justify-between mt-20 gap-12
-${({ direction }) => (direction === "left" ? "flex-row" : "flex-row-reverse")}
-`
+const Content = ({ section, observe, inView }: any) => (
+  <Container ref={observe} direction={section.direction}>
+    <Column>
+      <ImageContainer className={`${inView ? "" : "opacity-0"}`}>
+        <Image
+          src={section.imageUrl}
+          alt={section.imageAlt}
+          className={`${inView ? "" : "scale-125 rotate-2 translate-y-4"}`}
+        />
+      </ImageContainer>
+    </Column>
+    <Column>
+      <Text
+        variant="h5"
+        className={`transition duration-1000 delay-500 ${
+          inView ? "" : "opacity-0"
+        }`}
+      >
+        {section.featuresTitle}
+      </Text>
+      <div className="mt-12 space-y-8">
+        {section.features.map((feature, index) => (
+          <div
+            key={feature.title}
+            className={`transition transform duration-1000 ${
+              inView ? "" : "opacity-0 translate-y-8"
+            }`}
+            style={{
+              transitionDelay: 500 + index * 250 + "ms",
+            }}
+          >
+            <FeatureItem feature={feature} />
+          </div>
+        ))}
+      </div>
+    </Column>
+  </Container>
+)
 
 export const MidSection = ({ section }) => (
   <SectionAndOffset>
-    <TopPart section={section} />
-    <Container direction={section.direction}>
-      <Column>
-        <ImageContainer>
-          <Image src={section.imageUrl} alt={section.imageAlt} />
-        </ImageContainer>
-      </Column>
-      <Column>
-        <Text variant="h5">{section.featuresTitle}</Text>
-        <div className="mt-12 space-y-8">
-          {section.features.map(feature => (
-            <FeatureItem key={feature.title} feature={feature} />
-          ))}
-        </div>
-      </Column>
-    </Container>
+    <InView unobserveOnEnter>
+      <TopPart section={section} />
+    </InView>
+    <InView unobserveOnEnter>
+      <Content section={section} />
+    </InView>
   </SectionAndOffset>
 )
