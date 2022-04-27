f75478c0b84a5fae24b3a5d2dff60648cb30b5eb
Max <mymaksym@gmail.com>
2022-04-27 15:25:00 +0100

    animate bottom hero section

--- apps/lp/src/sections/home/bottom-hero.tsx
+++ apps/lp/src/sections/home/bottom-hero.tsx
@@ -1,13 +1,18 @@
 import React from "react"
 import { Button, SectionAndOffset, Text } from "components"
 import { LogoTitle } from "./hero"
+import { InView } from "react-cool-inview"
 
-export const BottomHeroSection = () => (
-  <SectionAndOffset>
+export const BottomHeroSectionCore = ({ observe, inView }: any) => (
+  <SectionAndOffset ref={observe}>
     <div className="flex flex-col items-center">
-      <LogoTitle />
+      <LogoTitle inView={inView} />
     </div>
-    <div className="flex flex-col items-center w-1/3 text-center mx-auto mt-8">
+    <div
+      className={`flex flex-col items-center w-1/3 mx-auto mt-8 text-center transition duration-700 delay-[1400ms] ${
+        inView ? "" : "opacity-0"
+      }`}
+    >
       <Text>
         {
           "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias ipsam at autem, aperiam doloribus beatae sed quasi distinctio reiciendis odio commodi!"
@@ -18,4 +23,10 @@ export const BottomHeroSection = () => (
       </div>
     </div>
   </SectionAndOffset>
+)
+
+export const BottomHeroSection = () => (
+  <InView>
+    <BottomHeroSectionCore />
+  </InView>
 )
