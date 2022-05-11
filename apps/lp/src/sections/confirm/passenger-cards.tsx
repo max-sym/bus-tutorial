752583be5872de46d684cab6fd60879c00911c0c
Max <mymaksym@gmail.com>
2022-05-11 15:40:58 +0100

    add formik

--- apps/lp/src/sections/confirm/passenger-cards.tsx
+++ apps/lp/src/sections/confirm/passenger-cards.tsx
@@ -1,8 +1,17 @@
 import React from "react"
 import { Card, Button, CardContent, Label, Text, InputCore } from "components"
-import { PassengerType, useStore } from "store"
+import { PassengerType, ReservationType, useStore } from "store"
+import { useFormik } from "formik"
 
-const PassengerCard = ({ passenger }: { passenger: PassengerType }) => (
+const PassengerCard = ({
+  passenger,
+  index,
+  formik,
+}: {
+  passenger: PassengerType
+  index: number
+  formik: any
+}) => (
   <Card>
     <CardContent>
       <Text>{passenger.id}</Text>
@@ -12,7 +21,8 @@ const PassengerCard = ({ passenger }: { passenger: PassengerType }) => (
           <InputCore
             placeholder="John Doe"
             value={passenger.name}
-            onChange={() => {}}
+            name={`passengers.${index}.name`}
+            onChange={formik.handleChange}
           />
         </div>
         <div>
@@ -20,15 +30,18 @@ const PassengerCard = ({ passenger }: { passenger: PassengerType }) => (
           <InputCore
             placeholder="john@example.com"
             value={passenger.email}
-            onChange={() => {}}
+            type="email"
+            name={`passengers.${index}.email`}
+            onChange={formik.handleChange}
           />
         </div>
         <div>
           <Label>{"Citizen ID"}</Label>
           <InputCore
             placeholder="XXXXXX"
+            name={`passengers.${index}.citizenId`}
             value={passenger.citizenId}
-            onChange={() => {}}
+            onChange={formik.handleChange}
           />
         </div>
       </div>
@@ -36,21 +49,44 @@ const PassengerCard = ({ passenger }: { passenger: PassengerType }) => (
   </Card>
 )
 
+const PassengersForm = ({ reservation }: { reservation: ReservationType }) => {
+  const formik = useFormik({
+    initialValues: {
+      passengers: reservation.passengers.map(passenger => ({
+        id: passenger.id,
+        name: passenger.name || "",
+        email: passenger.email || "",
+        citizenId: passenger.citizenId || "",
+      })),
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2))
+    },
+  })
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <div className="mt-4 space-y-4">
+        {formik.values.passengers.map((passenger, index) => (
+          <PassengerCard
+            key={passenger.id}
+            index={index}
+            passenger={passenger}
+            formik={formik}
+          />
+        ))}
+      </div>
+      <div className="mt-4">
+        <Button type="submit">{"Save"}</Button>
+      </div>
+    </form>
+  )
+}
+
 export const PassengerCards = () => {
   const reservation = useStore(store => store.reservation)
 
   if (!reservation?.passengers) return null
 
-  return (
-    <>
-      <div className="mt-4 space-y-4">
-        {reservation.passengers.map(passenger => (
-          <PassengerCard key={passenger.id} passenger={passenger} />
-        ))}
-      </div>
-      <div className="mt-4">
-        <Button>{"Save"}</Button>
-      </div>
-    </>
-  )
+  return <PassengersForm reservation={reservation} />
 }
