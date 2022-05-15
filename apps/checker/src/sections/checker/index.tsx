5e7097ff6bfe16a4a3ed04f6ed6131787fdd43d2
Max <mymaksym@gmail.com>
2022-05-15 18:52:35 +0100

    fix moment import in checker

--- apps/checker/src/sections/checker/index.tsx
+++ apps/checker/src/sections/checker/index.tsx
@@ -14,7 +14,7 @@ import {
   ModalType,
 } from "@bus/ui"
 import { Portal } from "@headlessui/react"
-import * as moment from "moment"
+import moment from "moment"
 import { useEffect, useState } from "react"
 import { useRef } from "react"
 import tw from "tailwind-styled-components"
